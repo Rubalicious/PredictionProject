51fc7efb1c4ac9f99cf789082ddd64068d327aab
Rubalicious <rabrams12@email.arizona.edu>
2015-08-04 18:13:10 -0700

    added grayness averages

--- data_analysis.m
+++ data_analysis.m
@@ -2,26 +2,30 @@
 
 
 %Make a table of the stats
-table = zeros(40,4);
-%making columns that are: ID, brightness, contrast, avg grayness
+table = zeros(40,5);
+%making columns that are: ID, brightness, contrast, range, avg grayness
 %reading happiness folder
 files = dir('picture data/happiness/*.jpg');
 i = 1;
 for file = files'
-    imread(file.name);
+    rgb = imread(file.name);
     brightness = mean2(file.name);
-    contrast = std(file.name);
+    contrast = std2(file.name);
     range = max(file.name)-min(file.name);
+    gray = rgb2gray(rgb);
+    avggray = mean2(gray);
     
     %populating table
     table(i,1) = i;
     table(i,2) = brightness;
     table(i,3) = contrast;
     table(i,4) = range;
+    table(i,5) = avggray;
     i = i+1;
 end
 figure
-scatter3(table(:,2), table(:,3), table(:,4),'*r');
+hold on
+scatter3(table(1:20,2), table(1:20,3),table(1:20,4),'*r');
 xlabel('brightness')
 ylabel('contrast')
 zlabel('range')
@@ -30,21 +34,39 @@ zlabel('range')
 files = dir('picture data/sadness/sad*');
 i = 21;
 for file = files'
-    imread(file.name);
+    rgb = imread(file.name);
     brightness = mean2(file.name);
-    contrast = std(file.name);    
+    contrast = std2(file.name);    
     range = max(file.name)-min(file.name);
+    gray = rgb2gray(rgb);
+    avggray = mean2(gray);
     
     %populating table
     table(i,1) = i;
     table(i,2) = brightness;
     table(i,3) = contrast;
     table(i,4) = range;
+    table(i,5) = avggray;
     i = i+1;
 end
-hold on
-scatter3(table(:,2), table(:,3), table(:,4),'ob');
+
+scatter3(table(21:40,2), table(21:40,3), table(21:40,4),'ob');
 hold off
 xlabel('brightness')
-ylabel('contrast')
-zlabel('range')
+%% some ideas
+%notice the difference between average grayness
+plot(table(1:20,5))
+hold on
+plot(table(21:40,5))
+xlabel('picture ID')
+ylabel('average grayness')
+legend('happy pictures', 'sad pictures')
+hold off
+%% plot the average of happy pictures and sad pictures
+hold on
+x = 1:20;
+y1 = mean(table(1:20,5));
+scatter(x,y1*ones(20,1),'b*');
+y2 = mean(table(21:40,5));
+scatter(x, y2*ones(20,1),'r');
+hold off
