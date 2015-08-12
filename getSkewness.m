b6ffe28939771be6f8cf5e5afdd935adf122cbf6
jtcelayaalcala <jtcelayaalcala@users.noreply.github.com>
2015-08-11 20:20:26 -0700

    Update getSkewness.m

--- getSkewness.m
+++ getSkewness.m
@@ -1,7 +1,3 @@
 function x = getSkewness(I)
-
-    x = (3*(mean2(I) - median2(I))/std2(I));
-
-%     x = (3*(mean2(I) - getMedian(I))/std2(I));
-
+    x = (3*(mean2(I) - getMedian(I))/std2(I));
 end
