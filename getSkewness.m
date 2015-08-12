61f69492946dfee7746aa6383e768ae03e5b98bd
Rubalicious <rabrams12@email.arizona.edu>
2015-08-11 20:17:14 -0700

    may have messed up getSkewness.m

--- getSkewness.m
+++ getSkewness.m
@@ -1,7 +1,7 @@
 function x = getSkewness(I)
-<<<<<<< HEAD
+
     x = (3*(mean2(I) - median2(I))/std2(I));
-=======
-    x = (3*(mean2(I) - getMedian(I))/std2(I));
->>>>>>> 6d08dd396a939525c350a844711bac6ee01858b4
+
+%     x = (3*(mean2(I) - getMedian(I))/std2(I));
+
 end
