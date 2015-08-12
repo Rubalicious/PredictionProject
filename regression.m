8b6cd6f6b45b95342aa6cb265f1c5ee18e674516
Jose Teodoro Celaya-Alcala <jtcelayaalcala@email.arizona.edu>
2015-08-11 18:07:30 -0700

    added kurtosis and skew functions

--- regression.m
+++ regression.m
@@ -3,10 +3,14 @@
 %Make a table of the stats
 table = zeros(40,5);
 %making columns that are: ID, brightness, contrast, range, avg grayness
+
 %reading happiness folder
 files = dir('picture data/happiness/*.jpg');
 i = 1;
+
 for file = files'
+    
+    %getting image statistics
     rgb = imread(file.name);
     brightness = mean2(file.name);
     contrast = std2(file.name);
@@ -22,18 +26,14 @@ for file = files'
     table(i,5) = avggray;
     i = i+1;
 end
-figure
-hold on
-scatter3(table(1:20,2), table(1:20,3),table(1:20,4),'*r');
-xlabel('brightness')
-ylabel('contrast')
-zlabel('range')
+
 %% sadness
 %reading sadness folder
 files = dir('picture data/sadness/sad*');
-i = 21;
+
 for file = files'
     
+    %getting image statistics
     rgb = imread(file.name);
     brightness = mean2(file.name);
     contrast = std2(file.name);    
@@ -49,24 +49,3 @@ for file = files'
     table(i,5) = avggray;
     i = i+1;
 end
-
-scatter3(table(21:40,2), table(21:40,3), table(21:40,4),'ob');
-hold off
-xlabel('brightness')
-%% some ideas
-%notice the difference between average grayness
-plot(table(1:20,5))
-hold on
-plot(table(21:40,5))
-xlabel('picture ID')
-ylabel('average grayness')
-legend('happy pictures', 'sad pictures')
-hold off
-%% plot the average of happy pictures and sad pictures
-hold on
-x = 1:20;
-y1 = mean(table(1:20,5));
-scatter(x,y1*ones(20,1),'b*');
-y2 = mean(table(21:40,5));
-scatter(x, y2*ones(20,1),'r');
-hold off
